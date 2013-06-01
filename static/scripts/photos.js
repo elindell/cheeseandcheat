fc75be9469bb26fc9721dfa7802fb53a0396c4f8
Evan Lindell <evan.lindell@gmail.com>
2013-06-01 00:21:40 -0500

    Grab photos from facebook

--- static/scripts/photos.js
+++ static/scripts/photos.js
@@ -1,3 +1,14 @@
 (function($) {
-	$('a.thumb').colorbox({rel:'gal', width:"75%", height:"75%"});
+	$('a.thumb').colorbox({rel:'gal', width:"90%", height:"90%"});
+	var fadeTimeMilliseconds = 650;
+	var toastDurationMilliseconds = 4000;
+	var toastDelayMilliseconds = 1000;
+	setTimeout(function() {
+		$('#toast').fadeIn(fadeTimeMilliseconds, function() { 
+			setTimeout(function() {
+				$('#toast').fadeOut(fadeTimeMilliseconds); 
+			}, toastDurationMilliseconds);
+		});
+	}, toastDelayMilliseconds);
+	
 })($);
