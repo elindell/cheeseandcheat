5612983132484de02d949784395b34a029bcf3b5
Evan Lindell <evan.lindell@gmail.com>
2013-06-01 22:40:18 -0500

    Add a 404 and a 500 page

--- handlers/http_errors.py
+++ handlers/http_errors.py
@@ -1,11 +1,17 @@
 import webapp2
+import jinja2
+import config
+
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(config.TEMPLATE_DIR))
 
 def handle_404(request, response, exception):
-    response.write('404d!')
+    template = jinja_environment.get_template('404.html')
+    response.out.write(template.render())
     response.set_status(404)
 
 def handle_500(request, response, exception):
-    response.write('500d..ed..!')
+    template = jinja_environment.get_template('500.html')
+    response.out.write(template.render())
     response.set_status(500)
 
 def add_handlers(app):
