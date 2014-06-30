949095b0204d6619c7e328b692242a110d20cdff
Evan Lindell <evan.lindell@gmail.com>
2014-06-29 22:44:33 -0700

    Add engagement photos...just in time for July, like we promised!

--- static/scripts/photos.js
+++ static/scripts/photos.js
@@ -1,14 +1,3 @@
 (function($) {
 	$('a.thumb').colorbox({rel:'gal', width:"90%", height:"90%"});
-	var fadeTimeMilliseconds = 650;
-	var toastDurationMilliseconds = 4000;
-	var toastDelayMilliseconds = 1000;
-	setTimeout(function() {
-		$('#toast').fadeIn(fadeTimeMilliseconds, function() { 
-			setTimeout(function() {
-				$('#toast').fadeOut(fadeTimeMilliseconds); 
-			}, toastDurationMilliseconds);
-		});
-	}, toastDelayMilliseconds);
-	
 })($);
